46372feb8672ac892ab443c9a482adf0e5af9ef6
LazyTurtleknight <tube3719@gmail.com>
2021-10-21 15:52:04 +0200

    Setup linter and add basic main function to read input from the console.

--- src/main/java/com/challenge/App.java
+++ src/main/java/com/challenge/App.java
@@ -1,13 +1,49 @@
 package com.challenge;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
- * Hello world!
- *
+ * Class containing main function.
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+  /**
+   * Main function. Reads input from the console.
+   * 
+   * @param args
+   * @throws IOException
+   */
+  public static void main(String[] args) throws IOException {
+    // variable to handle loop condition
+    Boolean condition = true;
+    // establish reader to read from console
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // intermediate variable to parse input
+    String input = "";
+    String[] inputComponents;
+    Integer i = 1;
+
+    while (condition) {
+
+      input = br.readLine();
+      try {
+        inputComponents = input.split("\\s+");
+        if (inputComponents.length == 3) {
+          i = 0;
+        }
+        for (String component : inputComponents) {
+          System.out.println(component);
+        }
+      } catch (NumberFormatException nfe) {
+        System.err.println("Invalid Format!");
+      }
+      if (i == 0) {
+        condition = false;
+      }
+
     }
+
+  }
 }
